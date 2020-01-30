c355254ad965b5e9d9073e53863961f09ee62ec5
Simon Hacks <shacks@kth.se>
2020-01-30 16:37:45 +0100

    Test for IO Server added

---
+++ src/test/java/org/mal_lang/epeslang/test/IOServerTest.java
@@ -0,0 +1,30 @@
+package org.mal_lang.epeslang.test;
+
+import core.Attacker;
+import org.junit.jupiter.api.Test;
+
+public class IOServerTest extends EpesLangTest {
+    @Test
+    public void testRestrictedOperationsNoDefense() {
+        Attacker attacker = new Attacker();
+        IOServer server = new IOServer(
+                false, false,
+                false, false,
+                false, false,
+                false
+        );
+        IcsNetwork network = new IcsNetwork(
+                false, false,
+                false, false,
+                false
+        );
+
+        network.addAssets(server);
+
+        attacker.addAttackPoint(server.externalRemoteService);
+        attacker.addAttackPoint(network.access);
+        attacker.attack();
+
+        server.restrictedOperations.assertCompromisedInstantaneously();
+    }
+}
