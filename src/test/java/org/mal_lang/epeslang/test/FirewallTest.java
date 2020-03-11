a0af0be3c6b33a11d4d56b2af652fd76c23b8491
Simon Hacks <shacks@kth.se>
2020-03-11 12:00:56 +0100

    Solving bug on commonly used port on ics asset leading to access on firewall

---
+++ src/test/java/org/mal_lang/epeslang/test/FirewallTest.java
@@ -0,0 +1,40 @@
+package org.mal_lang.epeslang.test;
+
+import core.Attacker;
+import org.junit.jupiter.api.Test;
+
+public class FirewallTest extends EpesLangTest {
+    @Test
+    public void testCommonlyUsedPortWithAssetNotReached() {
+        Attacker attacker = new Attacker();
+
+        Firewall firewall = new Firewall(
+                false, false,
+                false, false,
+                false, false,
+                false, false,
+                false
+        );
+
+        IcsNetwork network = new IcsNetwork(
+                false, false,
+                false, false,
+                false
+        );
+
+        HMI hmi = new HMI(
+                false, false,
+                false, false,
+                false, false,
+                false, false,
+                false
+        );
+
+        network.addApplications(hmi);
+        network.addApplications(firewall);
+
+        attacker.attack();
+
+        firewall.bypassFirewall.assertUncompromised();
+    }
+}
